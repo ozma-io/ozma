2383452a1048b8aee402131376f3413090e6c98a
Кирилл Маркин <Kirmark@MacBook-Kirill.local>
2017-05-04 18:41:23 +0300

    Теперь в форме меню выводится общим видом

--- src/Views/FormView.cs
+++ src/Views/FormView.cs
@@ -2,6 +2,7 @@ namespace FunWithFlags.FunApp.Views
 {
     using System.Dynamic;
     using System.Linq;
+    using System.Collections.Generic;
 
     using FunWithFlags.FunCore;
 
@@ -17,6 +18,40 @@ namespace FunWithFlags.FunApp.Views
             dynamic model = new ExpandoObject();
             
             model.View = uv;
+
+            model.Menubar = new List<ExpandoObject>();
+
+            dynamic tel = new ExpandoObject();
+            tel.Name = "Cущность 1";
+            tel.Link = "../nav";
+            tel.Sub = new List<ExpandoObject>();
+            model.Menubar.Add(tel);
+
+            dynamic tel2 = new ExpandoObject();
+            tel2.Name = "Все";
+            tel2.Link = "../uv/1";
+            tel2.Sub = new List<ExpandoObject>();
+            dynamic tel3 = new ExpandoObject();
+            tel3.Name = "Представление";
+            tel3.Link = "../uv/1";
+            for (int i = 0; i < 3; i++)
+            {
+                tel2.Sub.Add(tel3);
+            }
+            model.Menubar.Add(tel2);
+
+            dynamic t1 = new ExpandoObject();
+            t1.Name = "Основная";
+            t1.Link = "../uv/2";
+            t1.Sub = new List<ExpandoObject>();
+            dynamic t2 = new ExpandoObject();
+            t2.Name = "Форма";
+            t2.Link = "../uv/2";
+            for (int i = 0; i < 3; i++)
+            {
+                t1.Sub.Add(t2);
+            }
+            model.Menubar.Add(t1);
 
             return model;
         }
