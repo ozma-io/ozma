a6b74e7080048588ce77b925160b23f49d0e7133
Кирилл Маркин <Kirmark@MacBook-Kirill.local>
2017-05-09 14:46:40 +0300

    Доработка вывода таблицы

--- src/Views/TableView.cs
+++ src/Views/TableView.cs
@@ -25,6 +25,35 @@ namespace FunWithFlags.FunApp.Views
             dynamic model = new ExpandoObject();
             
             // Использовать фильтр UV
+            dynamic dbmodel = new ExpandoObject();
+            dbmodel.Entries = db.Entities.Where(e =>
+                db.UVEntities.Where(uve =>
+                    uve.EntityId == e.Id &&
+                    uve.UserViewId == uv.Id
+                ).Any()
+            ).GroupJoin(db.Fields,
+                ent => ent.Id,
+                fld => fld.EntityId,
+                (ent, fld) => new {
+                    Entity = ent,
+                    Fields = fld.Where(tf =>
+                        tf.EntityId == ent.Id
+                    ).ToList()
+                }
+            ).ToList();
+            
+            const string quote = "\"";
+            const string sl = "\\";
+
+            string[,] strs = new string[,] {};
+            for(int i = 0; i < dbmodel.Entries[0].Fields.Count; i++) {
+                strs[i,0] = dbmodel.Entries[0].Fields[i].Name;
+                strs[i,1] = string.Join(null, sl, quote, strs[i,0], sl, quote);
+            }
+
+            model.Entries = userDb.Query(dbmodel.Entries[0].Entity.Name, strs, "");
+
+            /*
             model.Entries = userDb.Query("Tests", new[]
                     {
                         Tuple.Create("Name", "\"Name\""),
@@ -32,7 +61,9 @@ namespace FunWithFlags.FunApp.Views
                         Tuple.Create("Description", "\"Description\""),
                         Tuple.Create("Param1", "\"Param1\""),
                         Tuple.Create("Param2", "\"Param2\""),
-                    }, "");
+                    }, ""
+            );
+             */
 
             model.Titles = db.UVFields.Where(uvf =>
                 uvf.UserViewId == uv.Id
