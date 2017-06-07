22ce66c49f96c77c9a2dcd86aca793de23cb441d
MarkinK <markinkirill@gmail.com>
2017-06-07 16:02:39 +0300

    Эксперимент с парамтерам в ссылке

--- src/Views/FormView.cs
+++ src/Views/FormView.cs
@@ -30,7 +30,7 @@ namespace FunWithFlags.FunApp.Views
             // !- разобраться как работать с параметрами
             var recId = (int)getPars.recId;
             // ! убрать
-            throw new NotImplementedException(recId.ToString());
+            //throw new NotImplementedException(recId.ToString());
             dynamic model = new ExpandoObject();
 
             model.Color = db.Settings.Single(s => s.Name == "bgcolor").Value;
