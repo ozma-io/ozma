d22ff9bd4187aa10edc6efd69c64e82ef2e552ba
dbreiten <nevisdale@gmail.com>
2019-07-17 12:14:27 +0300

    button-showNewRow in actions

--- src/components/views/Table.vue
+++ src/components/views/Table.vue
@@ -6,7 +6,7 @@
             "no": "No",
             "export_to_csv": "Export to .csv",
             "remove_selected_rows": "Remove selected rows",
-            "show_new_row": { "add": "Add new row", "rm": "Remove new row"}
+            "show_new_row": "Add/Remove new row"
         },
         "ru": {
             "clear": "Очистить",
@@ -14,7 +14,7 @@
             "no": "Нет",
             "export_to_csv": "Экспорт в .csv",
             "remove_selected_rows": "Удалить выбранные записи",
-            "show_new_row": {"add": "Добавить новую строку", "rm": "Убрать новую строку"} 
+            "show_new_row": "Добавить/Убрать новую строку"
         }
     }
 </i18n>
@@ -40,7 +40,7 @@
                     <tr>
                         <th class="fixed-column checkbox-cells"></th>
                         <th v-if="hasRowLinks" class="fixed-column opemform-cells links-style">
-                            <span @click="changeShowEmptyRow()" :title="this.$tc(`show_new_row.${showEmptyRow ? 'rm' : 'add'}`)">
+                            <span @click="changeShowEmptyRow()" :title="this.$tc('show_new_row')">
                                 {{ showEmptyRow ? "-" : "+" }}
                             </span>
                         </th>
@@ -682,6 +682,7 @@
             this.$emit("update:actions", [
                 { name: this.$tc("export_to_csv"), callback: () => this.export2csv() },
                 { name: this.$tc("remove_selected_rows"), callback: () => this.removeSelectedRows() },
+                { name: this.$tc("show_new_row"), callback: () => this.changeShowEmptyRow() },
             ])
             this.$emit("update:enableFilter", true)
 
