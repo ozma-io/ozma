771a2049b764af9d41280e83d5263c7910834300
nevisdale <nevisdale@gmail.com>
2019-08-09 00:23:33 +0300

    fix code style

--- src/components/FormControl.vue
+++ src/components/FormControl.vue
@@ -221,10 +221,10 @@
         }
 
         private beforeDestroy() {
-            if (this.inputType.name === "textarea") { 
-                this.updateValue(this.valueText.replace(/(\n+)|(\r+)|((\r\n)+)/gm, '\n').replace(/(\s+$)|(^\s+)/gm, ""))
+            if (this.inputType.name === "textarea") {
+                this.updateValue(this.valueText.replace(/(\n+)|(\r+)|((\r\n)+)/gm, "\n").replace(/(\s+$)|(^\s+)/gm, ""))
             } else if (this.inputType.name === "text") {
-                 this.updateValue(this.valueText.replace(/(\s+$)|(^\s+)/gm, ""))
+                this.updateValue(this.valueText.replace(/(\s+$)|(^\s+)/gm, ""))
             }
         }
 
