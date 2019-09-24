e482ddae7d07bee4bd85c6ee2066da2fa52de998
Dale <ipanferov@myprocessx.com>
2019-09-24 15:00:06 +0300

    feature: New Multiselect component

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,5 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from '@storybook/vue';
+import Vue from 'vue';
+import vClickOutside from 'v-click-outside';
+import VueI18n from "vue-i18n";
+
+Vue.use(vClickOutside);
+Vue.use(VueI18n);
 
 const req = require.context('../../src/stories', true, /.stories.ts?x$/);
 
