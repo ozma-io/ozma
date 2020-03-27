91cc49294f582dbf5334086bfc5b9de09d38cdf4
Nikolay Amiantov <ab@fmap.me>
2020-03-27 20:13:19 +0000

    Revert "feat(ozma-1157): Sync top navigation with layouts (pull request #450)"
    This reverts pull request #450.
    
    > ![](https://bitbucket.org/repo/MrpG4zp/images/1560344714-screenshot-localhost-8080-views-IlyaTest-Primary-Table-1585282383622.png)
    > ‌
    > 
    > ![](https://bitbucket.org/repo/MrpG4zp/images/3698813825-screenshot-localhost-8080-views-IlyaTest-Primary-Table-1585282396480.png)
    > ‌
    > 
    > ![](https://bitbucket.org/repo/MrpG4zp/images/2788325836-screenshot-localhost-8080-views-IlyaTest-Primary-Table-1585240600746.png)
    > ‌
    > 
    > ![](https://bitbucket.org/repo/MrpG4zp/images/1069097441-screenshot-localhost-8080-views-IlyaTest-Primary-Table-1585240528420.png)
    > ‌
    > 
    > ![](https://bitbucket.org/repo/MrpG4zp/images/2810459590-screenshot-localhost-8080-views-IlyaTest-Primary-Table-1585240522275.png)
    > ‌

--- src/modules.ts
+++ src/modules.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import VueRouter, {Route} from "vue-router";
+import VueRouter from "vue-router";
 import VueI18n from "vue-i18n";
 import Vuex from "vuex";
 import BootstrapVue from "bootstrap-vue";
