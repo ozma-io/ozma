fb209fd3f90bf088a66c7ea8e02146000dff8a05
Dale <ipanferov@myprocessx.com>
2020-05-19 11:29:48 +0300

    fix: Use valueType to chose correct component

--- src/components/views/table/TableCell.vue
+++ src/components/views/table/TableCell.vue
@@ -19,7 +19,7 @@
       @[indirectLinks?`click`:null]="$emit('goto', $event)"
     >
       <checkbox
-        v-if="typeof value.value === 'boolean'"
+        v-if="valueType === 'bool'"
         class="checkbox_click-none"
         :checked="value.value"
         disabled
@@ -30,7 +30,7 @@
     </UserViewLink>
     <template v-else>
       <checkbox
-        v-if="typeof value.value === 'boolean'"
+        v-if="valueType === 'bool'"
         class="checkbox_click-none"
         :checked="value.value"
         disabled
@@ -64,6 +64,10 @@ export default class TableCell extends Vue {
   @Prop({ type: String, default: "existing" }) from!: string;
   @Prop({ type: Number, default: null }) lastFixedColumnIndex!: number;
   @Prop({ type: Number, default: null }) index!: number;
+
+  get valueType(): string {
+    return this.value.info.field.valueType.type;
+  }
 
   get isNull() {
     // We use `value.value` here to highlight unvalidated values.
