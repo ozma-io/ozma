f3e815e3d25f6ce264d1885cc11ec9d06782b52f
dantes <baevdantes1993@gmail.com>
2020-05-27 21:14:46 +0500

    feat(ozma-1308): Progress bar

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,37 @@
+<template functional>
+  <div class="progressbar">
+    <div class="progressbar__line" />
+  </div>
+</template>
+
+<style scoped>
+  .progressbar {
+    position: fixed;
+    top: 0;
+    z-index: 999;
+    width: 100%;
+    height: 5px;
+    background: #dee2e6;
+  }
+
+  .progressbar__line {
+    width: 33.333%;
+    left: 0;
+    background: #000;
+    height: 100%;
+    animation: 1000ms move ease-in-out infinite;
+    transition: 500ms all linear;
+    position: absolute;
+  }
+
+  @keyframes move {
+    from {
+      left: -33.333%;
+    }
+
+    to {
+      width: 200%;
+      left: 100%;
+    }
+  }
+</style>
