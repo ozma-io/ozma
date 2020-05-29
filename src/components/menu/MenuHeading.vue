213bdc42b12e9837662506529d87186ed17d9f1d
Dale <ipanferov@myprocessx.com>
2020-05-29 15:05:48 +0300

    New headings for menu, ability to center menu, ability to specify 'icon' emoji for menu entries

---
+++ src/components/menu/MenuHeading.vue
@@ -0,0 +1,24 @@
+<template>
+  <component :is="`h${headingLevel}`">
+    <slot />
+  </component>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class MenuHeading extends Vue {
+  @Prop({ type: Number, default: 1 }) level!: number;
+
+  private get headingLevel(): number {
+    if (this.level < 1) {
+      return 1;
+    } else if (this.level > 6) {
+      return 6;
+    }
+    return this.level;
+  }
+}
+
+</script>
