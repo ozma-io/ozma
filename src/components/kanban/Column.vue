f1e41f05111bc169a13c0b0fa64742bceff4cb79
PhlBt <pheelborbot@gmail.com>
2020-09-08 12:49:24 +0400

    Show count cards and change spaces in cards on kanban

--- src/components/kanban/Column.vue
+++ src/components/kanban/Column.vue
@@ -17,7 +17,7 @@
           class="column_header__title"
           :title="title"
         >
-          {{ title }}
+          {{ title }} {{ cardCount }}
         </span>
         <span class="column_controls">
           <i
@@ -137,6 +137,10 @@ export default class Column extends Vue {
     return {
       maxWidth: strWidth,
     }
+  }
+
+  private get cardCount() {
+    return (this.cards.length > 0) ? `(${this.cards.length})` : '';
   }
 
   private get isAllSelected() {
