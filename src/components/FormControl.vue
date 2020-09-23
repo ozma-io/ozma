705d3b7db156de5ae3bd7cfef50106b64ee6e262
Ренат Дарыбаев <renatd.k@gmail.com>
2020-09-23 20:21:21 +0600

    custom-buttons-state-submit-reload

--- src/components/FormControl.vue
+++ src/components/FormControl.vue
@@ -465,7 +465,7 @@ export default class FormControl extends Vue {
   }
 
   get calendarValue() {
-    if (this.type.type === "datetime") {
+    if (this.type.type === "datetime" && this.currentValue !== null) {
       if (typeof this.currentValue === 'string') return this.currentValue;
       return this.currentValue.local().format("L LT");
     }
