f51dbfd6be0b693da6fc6e5c45f798fd8aec5df3
Ренат Дарыбаев <renatd.k@gmail.com>
2020-11-02 08:38:39 +0600

    Add qrcode file.

---
+++ src/components/qrcode/qrcode.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <qrcode :value="content" :options="{ width: height }"></qrcode>
+    <span><b>{{content}}</b></span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+
+@Component
+export default class QRCode extends Vue {
+  @Prop({ type: String }) content!: string;
+  @Prop({ type: Number, default: 200 }) height!: number;
+}
+
+</script>
