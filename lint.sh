49f7eafa6c51a978f005010c0f3e46597495b08c
Nikolay Amiantov <ab@fmap.me>
2020-12-12 10:51:21 +0000

    Actually fail on lint errors

--- lint.sh
+++ lint.sh
@@ -4,5 +4,6 @@
 set -x
 
 test -f yarn.lock
-yarn lint --no-fix
+yarn lint --no-fix --max-warnings 0
+# yarn lint:style doesn't return proper exit code when max-warnings is exceeded.
 npx stylelint --no-fix --max-warnings 0 src/
