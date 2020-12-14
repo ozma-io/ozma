5dcb000500e4cea6a09e3732e3ec1eed81cb0f83
Praron <koctr123@mail.ru>
2020-12-14 21:50:14 +0300

    Delete commented code

--- src/components/ActionsMenu.vue
+++ src/components/ActionsMenu.vue
@@ -313,10 +313,6 @@ export default class ActionsMenu extends Vue {
         text-align: left;
         z-index: 1000 !important; /* кнопка выбора действий выше темного блока */
       }
-
-      /* .right {
-       *   margin-left: -75vw;
-       * } */
     }
   }
 
