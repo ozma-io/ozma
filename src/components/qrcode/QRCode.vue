5a79e1cf6d193e7de013e8c9db375712cc0d9664
Ренат Дарыбаев <renatd.k@gmail.com>
2020-12-16 18:22:52 +0600

    QR-codes recognition. Print QRCode.

--- src/components/qrcode/QRCode.vue
+++ src/components/qrcode/QRCode.vue
@@ -1,17 +1,29 @@
 <template>
   <div v-if="content.length > 0">
-    <qrcode :value="content" :options="{ width: height }" />
-    <span><b>{{ content }}</b></span>
+    <qrcode :value="qrcodeContent" :options="{ width: height }" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import type { IEntriesRef } from "@/state/user_view";
+
+export interface IPrintQRCode {
+  s: string; // Schema
+  n: string; // Name
+  i: string; // ID
+}
 
 @Component
 export default class QRCode extends Vue {
   @Prop({ type: String, default: "" }) content!: string;
   @Prop({ type: Number, default: 200 }) height!: number;
+  @Prop({ type: Object }) entry!: IEntriesRef;
+
+  get qrcodeContent() {
+    const content: IPrintQRCode = { s: this.entry.entity.schema, n: this.entry.entity.name, i: this.content };
+    return JSON.stringify(content);
+  }
 }
 
 </script>
