90653c7a43fbb806a03036826a4a103a347c4beb
Ренат Дарыбаев <renatd.k@gmail.com>
2020-12-22 20:40:04 +0600

    Print barcode.

---
+++ src/components/barcode/BarCodePrint.vue
@@ -0,0 +1,15 @@
+<template>
+  <barcode :value="content">
+    Can not print Barcode!
+  </barcode>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import VueBarcode from "vue-barcode";
+
+@Component({ components: { "barcode": VueBarcode } })
+export default class BarCodePrint extends Vue {
+  @Prop({ type: String, default: "" }) content!: string;
+}
+</script>
