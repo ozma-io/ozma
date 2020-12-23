ccaa91afcefa919aea40e9e0518bf14c5dac78f1
Nikolay Amiantov <ab@fmap.me>
2020-12-23 08:10:47 +0000

    Fix style lint

--- lint.sh
+++ lint.sh
@@ -6,5 +6,4 @@ set -x
 test -f yarn.lock
 yarn
 yarn lint --no-fix --max-warnings 0
-# yarn lint:style doesn't return proper exit code when max-warnings is exceeded.
-npx stylelint --no-fix --max-warnings 0 src/
+yarn lint:style --no-fix --max-warnings 0
