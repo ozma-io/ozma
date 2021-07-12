dae53dd46e48d2b32f17879fef925058d1ac5a3c
kremizov <kremizov@ozma.io>
2021-07-12 15:41:41 +0300

    Lint fix

--- src/components/ArgumentEditor.vue
+++ src/components/ArgumentEditor.vue
@@ -83,7 +83,7 @@ import moment, { MomentInput } from "moment";
 import { ArgumentName, AttributesMap, FieldType, IArgument } from "ozma-api";
 import { objectMap } from "@/utils";
 import { valueIsNull, valueToText } from "@/values";
-import { attrObjectToQuery, IQuery } from "@/state/query";
+import { IQuery } from "@/state/query";
 
 const getValue = (parameter: IArgument, value: unknown) => {
   if (parameter.argType.type === "date" || parameter.argType.type === "datetime") {
