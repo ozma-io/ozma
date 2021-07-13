2b02ff7dcac35014efe67cb79cb61d3651f7ab4d
kremizov <kremizov@ozma.io>
2021-07-13 21:47:37 +0300

    Show toast on exceptions in actions

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -2,7 +2,19 @@ import { Dispatch } from "vuex";
 import { IActionRef, IActionResult } from "ozma-api";
 
 import Api from "@/api";
+import { app } from "@/main";
 import { CombinedTransactionResult } from "@/state/staging_changes";
+import { shortLanguage } from "@/utils";
+
+const messages: Record<string, Record<string, string>> = {
+  en: {
+    "exception_in_action": "Exception in action",
+  },
+  ru: {
+    "exception_in_action": "Исключение в действии",
+  },
+};
+const funI18n = (key: string) => messages[shortLanguage]?.[key]; // TODO: can't access VueI18n here, but this solution looks stupid too.
 
 export const saveAndRunAction = async (
   { dispatch }: { dispatch: Dispatch },
@@ -13,10 +25,24 @@ export const saveAndRunAction = async (
   let reloaded = false;
   try {
     const submitRet: CombinedTransactionResult[] = await dispatch("staging/submit", { preReload: async () => {
-      ret = await dispatch("callProtectedApi", {
-        func: Api.runAction,
-        args: [ref, args],
-      }, { root: true });
+      try {
+        ret = await dispatch("callProtectedApi", {
+          func: Api.runAction,
+          args: [ref, args],
+        }, { root: true });
+      } catch (e) {
+        const errorPrefix = "Error: Failed to run action: Unhandled exception ";
+        if (String(e).includes(errorPrefix, 0)) {
+          const error = String(e).replace(errorPrefix, "");
+          app.$bvToast.toast(error, {
+            title: funI18n("exception_in_action"),
+            variant: "danger",
+            solid: true,
+          });
+        }
+
+        throw e;
+      }
     } }, { root: true });
     reloaded = submitRet.length !== 0;
   } catch (e) {
