37670d5dd05d245620d21722ca48ecfb9e41bc4c
Nikolay Amiantov <ab@fmap.me>
2021-07-26 07:47:30 +0000

    Revert "Fix tab close button (pull request #972)"

--- src/components/modal/ModalTabHeader.vue
+++ src/components/modal/ModalTabHeader.vue
@@ -11,7 +11,9 @@
     @click="$emit('tab-click')"
   >
     <slot name="header" />
-    <ButtonItem v-if="!onlyTab" :button="closeButton" />
+    <!--
+    <ButtonItem :button="closeButton" />
+    -->
   </div>
 </template>
 
