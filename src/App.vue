f0574f7942afb264e36691db3b1c618c1790209e
kremizov <kremizov@ozma.io>
2021-11-11 21:34:24 +0300

    Add setting for GTM id and change GTM importing

--- src/App.vue
+++ src/App.vue
@@ -241,6 +241,18 @@ export default class App extends Vue {
       html.style.fontSize = `${this.fontSize}px`;
     }
 
+    const gtmId = this.settings.getEntry("google_tag_manager_container_id", String, null);
+    if (gtmId) {
+      const gtmScript = document.createElement("script");
+      gtmScript.type = "text/javascript";
+      gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`;
+      document.head.appendChild(gtmScript);
+    }
+
     this.loadColors();
   }
 
