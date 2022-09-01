8f16b93846ecc2b7a87a7682c29de073e0ea69f6
Nikolay Amiantov <ab@fmap.me>
2022-09-01 22:09:22 +0700

    Fail on build errors

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Build and publish the application.
 
+set -ex
+
 . ./prepare.sh
 rm -rf dist
 yarn build
--- lint.sh
+++ lint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Lint the application.
 
+set -ex
+
 . ./prepare.sh
 yarn lint --no-fix --max-warnings 0
 # Disable stylelint, as it often segfaults in CI.
