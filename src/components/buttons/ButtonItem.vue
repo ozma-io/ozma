90690ee43958083db2abfb53181b6c035110b902
Nikolay Amiantov <ab@fmap.me>
2022-11-12 12:35:19 +0700

    Properly handle actions for readonly instances

--- src/components/buttons/ButtonItem.vue
+++ src/components/buttons/ButtonItem.vue
@@ -13,18 +13,6 @@
     />
   </router-link>
 
-  <span
-    v-else-if="button.type === 'link' && button.link.type === 'action' && isReadonlyDemoInstance"
-    @click="onClickLinkReadonlyDemoInstance"
-  >
-    <ButtonView
-      :button="button"
-      :list-item="listItem"
-      :phantom-icon="listItemHasRightMargin"
-      :align-right="alignRight"
-    />
-  </span>
-
   <FunLink
     v-else-if="button.type === 'link'"
     :link="button.link"
@@ -78,16 +66,9 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
-import { namespace } from "vuex-class";
 
 import ButtonView from "@/components/buttons/ButtonView.vue";
 import type { Button } from "@/components/buttons/buttons";
-import { eventBus } from "@/main";
-import { CurrentSettings } from "@/state/settings";
-import { CurrentAuth, INoAuth } from "@/state/auth";
-
-const settings = namespace("settings");
-const auth = namespace("auth");
 
 @Component({
   components: {
@@ -99,20 +80,10 @@ export default class ButtonItem extends Vue {
   @Prop({ type: Boolean, default: false }) listItem!: boolean;
   @Prop({ type: Boolean, default: false }) listItemHasRightMargin!: boolean;
   @Prop({ type: Boolean, default: false }) alignRight!: boolean;
-  @settings.State("current") settings!: CurrentSettings;
-  @auth.State("current") auth!: CurrentAuth | INoAuth | null;
 
   private uploadFile(input: HTMLInputElement, next: (file: File) => void) {
     const files = input.files as FileList;
     next(files[0]);
-  }
-
-  private get isReadonlyDemoInstance() {
-    return this.settings.getEntry("is_read_only_demo_instance", Boolean, false) && this.auth?.refreshToken;
-  }
-
-  private onClickLinkReadonlyDemoInstance() {
-    eventBus.emit("show-readonly-demo-modal");
   }
 
   private onClickLink() {
