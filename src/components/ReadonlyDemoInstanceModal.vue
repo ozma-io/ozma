ca1e240d450d2173f2beebe5dde07b930a73ef86
Remizov Konstantin <kremizov64@gmail.com>
2024-01-10 21:19:22 +0700

    Update styles for readonly-demo-instance modal

--- src/components/ReadonlyDemoInstanceModal.vue
+++ src/components/ReadonlyDemoInstanceModal.vue
@@ -1,13 +1,11 @@
 <i18n>
     {
         "en": {
-            "header_1": "Request a demo call",
+            "header_1": "Schedule a demo call",
             "header_2": "to continue",
-            "description_1": "It's read-only example. To start making changes,",
-            "description_2": "contact our team, and we will create a personal demonstration instance just for you.",
-            "get_started": "Get in touch",
-            "sign_up": "Sign up (only if you know SQL and JS)",
-            "later": "Later"
+            "description_1": "It's read-only example. To start making changes, contact our team, and we will create a personal demonstration instance just for you.",
+            "get_started": "Demo call",
+            "sign_up": "Sign up (only if you know SQL and JS)"
         },
         "ru": {
             "header_1": "Свяжитесь с ozma.io",
@@ -15,8 +13,7 @@
             "description_1": "Это демо-пример. Чтобы начать вносить изменения,",
             "description_2": "свяжитесь с менеджером ozma.io.",
             "get_started": "Связаться с менеджером ozma.io",
-            "sign_up": "Зарегистрироваться (только если вы знаете SQL и JS)",
-            "later": "Посмотреть ещё"
+            "sign_up": "Зарегистрироваться (только если вы знаете SQL и JS)"
         },
         "es": {
             "header_1": "Conéctate con ozma.io",
@@ -24,8 +21,7 @@
             "description_1": "Es un ejemplo de solo lectura. Para comenzar, haga cambios,",
             "description_2": "haga una cita con un gerente y crearemos una instancia personal para usted.",
             "get_started": "Hacer una cita con el gerente",
-            "sign_up": "Registrarse (solo si sabes SQL y JS)",
-            "later": "Ver más"
+            "sign_up": "Registrarse (solo si sabes SQL y JS)"
         }
     }
 </i18n>
@@ -43,16 +39,25 @@
     @closed="showOverlay = false"
   >
     <div class="demo-message-container">
-      <!-- <i class="material-icons demo-icon">waving_hand</i> -->
-      <h1 class="demo-header">
-        {{ $t("header_1") }}<br>{{ $t("header_2") }}
-      </h1>
-      <span class="demo-message">
-        {{ $t("description_1") }}<br>{{ $t("description_2") }}
-      </span>
+      <div
+        class="material-button close-button"
+        @click="hide"
+      >
+        <i class="material-icons">close</i>
+      </div>
+      <i class="material-icons demo-icon">error_outline</i>
+      <div class="demo-message">
+        <h5 class="demo-header">
+          {{ $t("header_1") }} {{ $t("header_2") }}
+        </h5>
+        <span class="demo-body">
+          {{ $t("description_1") }}
+        </span>
+      </div>
       <div class="buttons-container">
         <b-button
           class="ok-button"
+          block
           variant="primary"
           :href="getStartedLink"
           target="_blank"
@@ -70,16 +75,6 @@
           {{ $t("sign_up") }}
         </b-button>
         -->
-
-        <b-button
-          class="cancel-button"
-          variant="outline-secondary"
-          @click="hide"
-        >
-          <a>
-            {{ $t("later") }}
-          </a>
-        </b-button>
       </div>
     </div>
   </ModalWindow>
@@ -131,13 +126,17 @@ export default class ReadonlyDemoInstanceModal extends Vue {
 
 <style lang="scss" scoped>
   .demo-modal ::v-deep > .vm--overlay {
-    background: rgba(0, 0, 0, 0.8) !important;
+    background: #1E1E1E !important;
+    opacity: 0.4;
   }
 
   .demo-modal ::v-deep > .vm--modal {
-    max-height: 80% !important;
-    max-width: 96% !important;
-    margin: 0 2% !important;
+    max-height: 80%;
+    max-width: min(90%, 28rem);
+    margin: 0 5%;
+
+    border-radius: 0.5rem;
+    box-shadow: none;
   }
 
   ::v-deep {
@@ -159,40 +158,73 @@ export default class ReadonlyDemoInstanceModal extends Vue {
   }
 
   .demo-message-container {
-    padding: 4rem 1rem;
+    padding: 1.75rem 2rem;
     display: flex;
+    gap: 1.5rem;
     flex-flow: column;
     align-items: center;
     overflow-y: auto;
   }
-
-  .demo-icon {
-    margin-top: 1rem;
-    margin-bottom: 2rem;
-    font-size: 5rem;
-    line-height: 6rem;
-    color: var(--default-foregroundDarkerColor);
+  @media screen and (max-width: 480px) {
+    .demo-message-container {
+      padding: 1.25rem 1rem;
+    }
   }
 
-  .demo-header {
-    margin-bottom: 1rem;
-    text-align: center;
-    font-weight: bold;
+  .close-button {
+    position: absolute;
+    top: 0.625rem;
+    right: 0.625rem;
+    width: 1.5rem;
+    height: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(229, 229, 229, 0.50);
+    color: #777C87;
+    border-radius: 50%;
+
+    .material-icons {
+      font-size: 1rem;
+    }
+  }
+
+  .demo-icon {
+    font-size: 3rem;
+    line-height: 3rem;
+    color: #777C87;
   }
 
   .demo-message {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .demo-header {
     text-align: center;
+    font-weight: 600;
+    color: #1F1F1F;
+  }
+
+  .demo-body {
+    font-size: 0.875rem;
+    text-align: center;
+    color: #3D3D3D;
+    opacity: 0.8;
   }
 
   .buttons-container {
-    margin-top: 2rem;
-    margin-bottom: 1rem;
+    width: 100%;
     display: flex;
-    flex-flow: column;
+    flex-direction: column;
   }
 
   .ok-button {
-    margin-bottom: 1.5rem;
-    font-weight: bold;
+    padding: 1rem;
+    background-color: #2361FF;
+    border-radius: 0.5rem;
+    &:hover {
+      background-color: #163da3;
+    }
   }
 </style>
