021fed808d77729bfd0fe7f09d34d1b8eaf920cd
Nikolay Amiantov <ab@fmap.me>
2024-09-10 21:51:03 +0000

    Reformat

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+import prettier_plugin_css_order from 'prettier-plugin-css-order'
+
+const config = {
+  "trailingComma": "all",
+  "semi": false,
+  "singleQuote": true,
+  "plugins": [prettier_plugin_css_order]
+}
+
+export default config
