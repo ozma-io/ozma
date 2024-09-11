59ef6b08521e1fb547d87224375b45dcd0909f73
Nikolay Amiantov <ab@fmap.me>
2024-09-11 15:46:58 +0000

    Fix the configs

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,13 @@
-'use strict'
+import * as fs from 'fs'
+import webpack from 'webpack'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import MonacoWebpackPlugin from 'monaco-editor-webpack-plugin'
 
-const { IgnorePlugin } = require('webpack')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
-
-const defaultConfig = require('./config/development.json')
+const defaultConfig = fs.readFileSync('./config/development.json')
 const configName = process.env['CONFIG'] || process.env['NODE_ENV']
 let buildConfig
 try {
-  buildConfig = require(`./config/${configName}.json`)
+  buildConfig = fs.readFileSync(`./config/${configName}.json`)
 } catch (e) {
   buildConfig = defaultConfig
 }
@@ -21,74 +20,64 @@ const defaults = {
 }
 
 const analyzeBundle = process.env['ANALYZE']
-const outputDir = process.env['OUTDIR'] || 'dist'
-const enableLint = process.env['NODE_ENV'] !== 'production'
 
-module.exports = {
-  assetsDir: 'static',
-  outputDir,
-
-  productionSourceMap: false,
-
-  lintOnSave: enableLint,
-
-  pluginOptions: {
-    lintStyleOnBuild: enableLint,
-    stylelint: {
-      fix: false,
-    },
-
-    i18n: {
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: true,
-    },
+export const assetsDir = 'static'
+export const outputDir = process.env['OUTDIR'] || 'dist'
+export const productionSourceMap = false
+export const lintOnSave = process.env['NODE_ENV'] !== 'production'
+export const pluginOptions = {
+  lintStyleOnBuild: lintOnSave,
+  stylelint: {
+    fix: false,
   },
 
-  configureWebpack: {
-    plugins: [
-      new MonacoWebpackPlugin({
-        languages: ['sql', 'javascript', 'json', 'html'],
-      }),
-      new IgnorePlugin({
-        resourceRegExp: /^\.\/locale$/,
-        contextRegExp: /moment$/,
-      }),
-      ...(analyzeBundle ? [new BundleAnalyzerPlugin()] : []),
-    ],
-  },
-
-  chainWebpack: (config) => {
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap((options) => {
-        options.compiler = require('vue-template-babel-compiler')
-        return options
-      })
-    /* Manually set prefetched chunks */
-    config.plugins.delete('prefetch')
-    config
-      .plugin('define')
-      .tap(([definitions, ...rest]) => [
-        { ...definitions, ...defaults, ...buildConfig },
-        ...rest,
-      ])
-    config.module
-      .rule('i18n')
-      .resourceQuery(/blockType=i18n/)
-      .type('javascript/auto')
-      .use('i18n')
-      .loader('@intlify/vue-i18n-loader')
-      .end()
-  },
-
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `@use "sass:math"; @import "~@/styles/mixins.scss";`,
-        sassOptions: { quietDeps: true },
-      },
+  i18n: {
+    fallbackLocale: 'en',
+    localeDir: 'locales',
+    enableInSFC: true,
+  }
+}
+export const configureWebpack = {
+  plugins: [
+    new MonacoWebpackPlugin({
+      languages: ['sql', 'javascript', 'json', 'html'],
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/,
+    }),
+    ...(analyzeBundle ? [new BundleAnalyzerPlugin()] : []),
+  ],
+}
+export const chainWebpack = (config) => {
+  config.module
+    .rule('vue')
+    .use('vue-loader')
+    .tap((options) => {
+      options.compiler = require('vue-template-babel-compiler')
+      return options
+    })
+  /* Manually set prefetched chunks */
+  config.plugins.delete('prefetch')
+  config
+    .plugin('define')
+    .tap(([definitions, ...rest]) => [
+      { ...definitions, ...defaults, ...buildConfig },
+      ...rest,
+    ])
+  config.module
+    .rule('i18n')
+    .resourceQuery(/blockType=i18n/)
+    .type('javascript/auto')
+    .use('i18n')
+    .loader('@intlify/vue-i18n-loader')
+    .end()
+}
+export const css = {
+  loaderOptions: {
+    sass: {
+      additionalData: `@use "sass:math"; @import "~@/styles/mixins.scss";`,
+      sassOptions: { quietDeps: true },
     },
   },
 }
