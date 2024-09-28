1391bfbce51a0a24f366796d1a6cee0269fddecc
Nikolay Amiantov <ab@fmap.me>
2024-09-28 21:56:02 +0700

    Allow to set the admin email

--- docker/keycloak-entrypoint.sh
+++ docker/keycloak-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 mkdir -p /opt/keycloak/data/import
 sed /etc/keycloak/realm.json \
   -e 's,{EXTERNAL_ORIGIN},'"$EXTERNAL_ORIGIN"',g' \
+  -e 's,{ADMIN_EMAIL},'"$ADMIN_EMAIL"',g' \
   > /opt/keycloak/data/import/realm.json
 
 exec /opt/keycloak/bin/kc.sh "$@"
