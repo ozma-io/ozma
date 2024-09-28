ffc68b7ffcad1a8fa5d21a8b54a4f9e3170bb0b2
Nikolay Amiantov <ab@fmap.me>
2024-09-28 19:30:14 +0700

    Enable the document generator

--- docker/keycloak-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir -p /opt/keycloak/data/import
-sed /etc/keycloak/realm.json \
-  -e 's,{OZMA_REPORT_GENERATOR_CLIENT_SECRET},'"$OZMA_REPORT_GENERATOR_CLIENT_SECRET"',g' \
-  > /opt/keycloak/data/import/realm.json
-
-exec /opt/keycloak/bin/kc.sh "$@"
--- docker/postgres-init-db.sh
+++ docker/postgres-init-db.sh
@@ -1,13 +1,19 @@
 #!/bin/sh -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname postgres <<-EOSQL
-  CREATE USER keycloak WITH PASSWORD 'keycloak';
-  CREATE DATABASE keycloak OWNER keycloak;
+  CREATE USER "keycloak" WITH PASSWORD 'keycloak';
+  CREATE DATABASE "keycloak" OWNER "keycloak";
   \c keycloak
-  ALTER SCHEMA public OWNER TO keycloak;
+  ALTER SCHEMA public OWNER TO "keycloak";
 
-  CREATE USER ozmadb WITH PASSWORD 'ozmadb';
-  CREATE DATABASE ozmadb OWNER ozmadb;
+  CREATE USER "ozmadb" WITH PASSWORD 'ozmadb';
+  CREATE DATABASE "ozmadb" OWNER "ozmadb";
   \c ozmadb
-  ALTER SCHEMA public OWNER TO ozmadb;
+  ALTER SCHEMA public OWNER TO "ozmadb";
+
+  CREATE USER "ozma-report-generator" WITH PASSWORD 'ozma-report-generator';
+  CREATE DATABASE "ozma-report-generator" OWNER "ozma-report-generator";
+  \c ozma-report-generator
+  ALTER SCHEMA public OWNER TO "ozma-report-generator";
+
 EOSQL
